160fcf17cc19890561e1ad823e5bc2f9b9561692
Farrukh Pashazadeh <farrukhpashazadeh@gmail.com>
2021-04-12 21:39:42 +0400

    ads-section added

--- dist/assets/js/adsdetails.js
+++ dist/assets/js/adsdetails.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
     owl = $("#owl-demo");
 
     owl.owlCarousel({
-
         navigation: false, // Show next and prev buttons
         slideSpeed: 300,
         paginationSpeed: 400,
